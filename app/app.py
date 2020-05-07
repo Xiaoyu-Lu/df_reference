9c82547d8bb80fbb31816d7e84c0e2a15b769983
Xy-Lu <loewilu@brandeis.edu>
2020-05-07 12:05:01 -0400

    update

---
+++ app/app.py
@@ -0,0 +1,122 @@
+import os
+import sys
+import json
+import random
+import string
+import copy
+from flask import Flask
+from flask import request
+from db import update_user_parameters
+from db import remove_user_data
+
+
+app = Flask(__name__)
+
+def is_browser(ua_string):
+    return ua_string.split("/")[0].lower() == "mozilla"
+
+@app.route("/", methods=["POST", "GET"])
+def process_dialogflow_request():
+    # show welcome message if viewing from a broswer
+    msg_content = "This is the webhook of Dom&Loewi."
+    if is_browser(request.headers["User-Agent"]):
+        return "<html><body><h1>{}</body></html>".format(msg_content)
+
+    # process request
+    query_text = request.json["queryResult"]["queryText"]
+    parameters = request.json["queryResult"]["parameters"]
+    intent = request.json["queryResult"]["intent"]["displayName"]
+    session = request.json["session"]
+
+    # process based on the domain
+    if "Attraction" in intent:
+        return process_attraction(query_text, parameters, intent, session)
+
+    # remove user data if the session is ended
+    if intent == "end of conversation":
+        remove_user_data(session)
+
+        return {
+            "fulfillmentText": random.choice([
+                "Thank you for using our service!",
+                "You're welcome, have a nice day!",
+                "Thank you! We're looking forward to assisting you again."
+            ])
+        }
+    
+    # the code reaches here when webhook is enabled on an intent
+    # but this intent is not yet included in our backend
+    return json.dumps(
+        {
+            "fulfillmentText": "Sorry, there is something wrong."
+        }
+    )
+
+def is_empty_parameter_dict(parameters):
+    """
+    Check if the current parameter dict is empty.
+
+    If the user does not give any information, we need to prompt 
+    the user to give more
+    """
+
+    for entity_name in parameters:
+        # if anything is not empty, the entire dict is not empty
+        if len(parameters[entity_name].strip()) > 0:
+            return False
+    
+    return True
+
+
+
+
+def process_attraction(query_text, parameters, intent, session):
+    """
+    This method handle all requests about attraction
+    """
+
+    continue_search = False
+    if intent == "add hotel info":
+        # update parameters
+        updated_user_profile = update_user_parameters(parameters, session, ignore_empty=True)
+
+        if not updated_user_profile:
+            # suggest parameters that the user has not yet given
+            return json.dumps(
+                {
+                    "fulfillmentText": random.choice(
+                        [
+                            "Sorry, but the information you gave is already"
+                            + " in the request. Would you mind adding another"
+                            + " piece of information?",
+                            "The information is already given. Mind adding"
+                            + " some other information?",
+                            "Sorry, but the information is already given. "
+                            + "Could you add some other information?",
+                        ]
+                    )
+
+                
+                }
+            )
+
+        continue_search = True
+
+
+    if intent == "Attraction-Recommend - refine_search":
+
+        # update parameters
+        # since this is updating information
+        # we should only update parameters that are NOT EMPTY
+        updated_user_profile = update_user_parameters(parameters, session, ignore_empty=True)
+
+        # only continue search with the updated parameters if there is anything new
+        # given by the user
+        if not updated_user_profile:
+            continue_search = True
+
+if __name__ == "__main__":
+    port = os.environ.get(
+        "PORT", int(sys.argv[1]) if len(sys.argv) >= 2 else 5000
+    )
+    app.run(host="0.0.0.0", port=port)
---
+++ app/db.py
@@ -0,0 +1,139 @@
+"""
+This file has all the functionality that works with database.
+Everything interaction with the database happens here.
+
+The current working directory is the project root.
+Please do not write any code that runs in the app folder.
+"""
+
+import json
+import copy
+import os
+
+# load data files
+attraction_db_file = open('app/attraction_db.json')
+
+# create user db if it does not exist
+if not os.path.isfile('app/user_db.json'):
+    with open('app/user_db.json', "w+") as user_db_file:
+        json.dump([], user_db_file, indent=4)
+user_db_file = open('app/user_db.json')
+
+ATTRACTION_DB = json.load(attraction_db_file)
+USER_DB = json.load(user_db_file)
+
+attraction_db_file.close()
+user_db_file.close()
+
+# build dict for more structured logic
+DATATYPE_TO_DB = {
+    "attraction": ATTRACTION_DB,
+    "user": USER_DB
+}
+
+
+def get_user_profile(session):
+    """
+    Get the user profile for the current session.
+    If it does not exit, create one and store it into the database.
+    """
+
+    global DATATYPE_TO_DB
+
+    result = None
+
+    # create one if it does not exist
+    for document in DATATYPE_TO_DB["user"]:
+        if document["session"] == session:
+            result = document
+            break
+    if result is None:
+        new_user_profile = {
+            "session": session,
+            # results will store the previously matched documents
+            # and it is a dict, where the key is the data type
+            # and the value is the matched documents.
+            # If the value is None, it means:
+            # 1) there was no match
+            # 2) there are too many matched documents currently
+            "results": {
+                "attraction": None,
+            },
+            # stores all parameters that the user has given
+            # regardless of data type.
+            "parameters": dict(),
+        }
+        DATATYPE_TO_DB["user"].append(new_user_profile)
+
+        # write to the database
+        with open('app/user_db.json', "w+") as user_db_file:
+            json.dump(DATATYPE_TO_DB["user"], user_db_file, indent=4)
+
+        result = new_user_profile
+
+    # use deep copy to prevent unwanted update of DB file
+    return copy.deepcopy(result)
+
+def update_user_parameters(dialogflow_parameters, session, ignore_empty=True):
+    """
+    Update the stored parameters for the current user.
+
+    Entity matches with empty results will be ignored. Every non-empty 
+    parameter will overwrite what are in the database. Unless specified
+    with option
+
+    Return the updated user profile. Return None if nothing is updated since:
+    1) everything given by the request is the same as what is in the DB
+    2) no entity was matched for the current sentence said by the user
+    """
+
+    global DATATYPE_TO_DB
+
+    # get the user profile and also make sure 
+    # there exists a user profile to update
+    user_profile = get_user_profile(session)
+
+    # update non-empty parameters
+    has_anything_new = False
+    for entity_name in dialogflow_parameters:
+        if (
+            entity_name not in user_profile["parameters"]
+            or (
+                (not ignore_empty or len(dialogflow_parameters[entity_name].strip()) > 0)
+                and dialogflow_parameters[entity_name] != user_profile["parameters"][entity_name]
+            )
+        ): 
+            user_profile["parameters"][entity_name] = dialogflow_parameters[entity_name]
+            has_anything_new = True
+
+    # no need to update the databse if nothing is new
+    if not has_anything_new:
+        return None
+
+    # search and update the document in database
+    for index, document in enumerate(DATATYPE_TO_DB["user"]):
+        if document["session"] == session:
+            DATATYPE_TO_DB["user"][index] = user_profile
+            break
+
+    # rewrite the databse file
+    with open('app/user_db.json', "w+") as user_db_file:
+        json.dump(DATATYPE_TO_DB["user"], user_db_file, indent=4)
+
+    return user_profile
+
+
+def remove_user_data(session):
+    """
+    After the conversation ends, delete the stored profile
+    for the current user.
+    """
+
+    # get the user profile
+    user_profile = get_user_profile(session)
+
+    DATATYPE_TO_DB["user"].remove(user_profile)
+
+    # rewrite the db file
+    with open('app/user_db.json', "w+") as user_db_file:
+        json.dump(DATATYPE_TO_DB["user"], user_db_file, indent=4)
